8cc71fb3245a7574678240213bb9737bbed523ff
papasmf1 <papasmf1@gmail.com>
2022-10-12 16:38:08 +0900

    함수연습4

--- function2.py
+++ function2.py
@@ -48,3 +48,14 @@ def change(x):
 #호출
 change(wordlist)
 print("함수 호출후:{0}".format(wordlist))
+
+print("---불변형식---")
+a = 1.2 
+print( id(a) )
+a = 2.3 
+print( id(a) )
+print("---가변형식---")
+lst = [1,2,3]
+print( id(lst) )
+lst.append(4)
+print( id(lst) )
