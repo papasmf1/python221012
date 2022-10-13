ed54dc5b4aea20c316672274c26514410d54a8bb
papasmf1 <papasmf1@gmail.com>
2022-10-13 15:06:03 +0900

    이름 숨김 연습

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,15 @@
+#전역변수
 str = "Not Class Member"
+
 class GString:
     def __init__(self):
+        #인스턴스 멤버 변수 초기화 
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        #개발자 실수
+        print(self.str)
 
 g = GString()
 g.set("First Message")
--- 생성자소멸자_참조카운트.py
+++ 생성자소멸자_참조카운트.py
@@ -1,16 +1,18 @@
 # -*- 생성자와 소멸자 그리고 참조 카운트 관리  -*-
 class MyClass:
+    #초기화매서드
     def __init__(self, value):
         self.value = value
         print("Instance is created! Value = ", value)
+    #소멸자메서드 
     def __del__(self):
         print("Instance is deleted!")
 
 #인스턴스 생성 
 d = MyClass(10)
-d_copy = d
-del d_copy 
-del d
+# del d 
+
+print("---전체 코드 실행 종료---")
 
 
 
