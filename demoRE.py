4f1e870f4d57ddc3ba1521f16a952e9f7f5928a3
papasmf1 <papasmf1@gmail.com>
2022-10-14 14:03:48 +0900

    정규표현식 연습1

---
+++ demoRE.py
@@ -0,0 +1,13 @@
+# demoRE.py 
+#정규표현식(특정한 패턴을 검색)
+import re 
+
+#포함하고 있는 경우 
+result = re.search("[0-9]*th", "35th")
+print( result )
+print( result.group() )
+
+#정확하게 일치하는 경우만
+result = re.match("[0-9]*th", "35th")
+print( result )
+print( result.group() )
