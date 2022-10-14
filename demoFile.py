6cbd1d3e17ca6d82bc0d07b1facde485ad26639c
papasmf1 <papasmf1@gmail.com>
2022-10-14 11:46:08 +0900

    파일 연습 완료

--- demoFile.py
+++ demoFile.py
@@ -11,4 +11,28 @@ print('---정렬변경')
 for x in range(1,6):
     print(x,"*",x,"=", str(x*x).rjust(3))
 
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(1500000))
 
+#파일 쓰기(유니코드로 저장)
+f = open("c:\\work\\demo.txt", "wt", encoding="utf-8")
+f.write("하나\n둘\n셋\n")
+f.close() 
+
+#파일 읽기
+f = open("c:\\work\\demo.txt", "rt", encoding="utf-8")
+print( f.read() )
+#어디쯤 읽고 있어? 
+print( f.tell() )
+#리셋(다시 처음)
+f.seek(0)
+print("---readline()---")
+print( f.readline(), end="" )
+print( f.readline(), end="" )
+#리셋(다시 처음)
+f.seek(0)
+result = f.readlines()
+print(result)
+
+f.close() 
